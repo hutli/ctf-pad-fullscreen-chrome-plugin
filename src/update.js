6511964ab33b8f6ff4987624fb6d8749d6bb4e7b
Jens Hegner Stærmose <jensstaermose@hotmail.com>
2021-08-11 19:55:54 +0200

    Skipping resizing on wrong sites

--- src/update.js
+++ src/update.js
@@ -1,6 +1,8 @@
 function fullscreen() {
     let epframecontent = document.getElementById("epframecontent");
-    epframecontent.style.height = "calc(100vh - 130px)";
+    if(epframecontent){
+        epframecontent.style.height = "calc(100vh - 130px)";
+    }
 }
 
 window.addEventListener('hashchange', fullscreen);
